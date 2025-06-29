3b1d972d70c5fef9e44a2d06cc75dccfc9d6877e
Matthieu Tinnes <matthieu.tinnes@gmail.com>
2025-06-29 13:46:27 +0200

    Update response codes for add_game and remove_game endpoints

--- steamkeyvault/games/api.py
+++ steamkeyvault/games/api.py
@@ -18,23 +18,23 @@ class GameOut(Schema):
     name: str
     steamappid: Optional[int] = None
 
-@router.post('/add', response={200: GameOut, 400: dict}, auth=django_auth)
+@router.post('/add', response={201: None, 400: dict}, auth=django_auth)
 def add_game(request, data: GameIn):
     if not data.name:
         return 400, {"error": "Name is required."}
     game, _ = Game.objects.get_or_create(name=data.name, defaults={"steamappid": data.steamappid})
     UserGame.objects.get_or_create(user=request.user, game=game)
-    return 200, GameOut(id=game.id, name=game.name, steamappid=game.steamappid)
+    return 201, None
 
 @router.get('/list', response=list[GameOut],auth=django_auth)
 def list_games(request):
     user_games = UserGame.objects.filter(user=request.user).select_related('game')
     return [GameOut(id=ug.game.id, user_game_id=ug.id , name=ug.game.name, steamappid=ug.game.steamappid) for ug in user_games]
 
-@router.delete('/remove/{user_game_id}', response={200: dict, 404: dict}, auth=django_auth)
+@router.delete('/remove/{user_game_id}', response={204: None, 404: dict}, auth=django_auth)
 def remove_game(request, user_game_id: int):
     user_game_qs = UserGame.objects.filter(user=request.user, user_game_id=user_game_id)
     if not user_game_qs.exists():
         return 404, {"error": "Game not found for this user."}
     user_game_qs.delete()
-    return 200, {"success": True}
+    return 204, None
