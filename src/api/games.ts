83fa63dd75ef44970b2aa9c32c68d48effa55499
Matthieu Tinnes <matthieu.tinnes@gmail.com>
2025-06-29 12:09:26 +0200

    feat: refactor searchSteamGames function to use axios for API calls and improve error handling

--- src/api/games.ts
+++ src/api/games.ts
@@ -3,10 +3,9 @@ import { API_BASE_URL, getCookie, getCSRFHeaders, getAxiosConfig } from './apiHe
 
 export async function searchSteamGames(query: string) {
   if (!query.trim()) return []
-  const res = await fetch(`${API_BASE_URL}/steam/search/?name=${encodeURIComponent(query)}`)
-  if (!res.ok) throw new Error('API error')
-  const data = await res.json()
-  return data
+  const headers = await getCSRFHeaders()
+    return axios.get(`${API_BASE_URL}/steam/search/?name=${encodeURIComponent(query)}`, getAxiosConfig(headers))
+    .then(response => response.data);
 }
 
 export async function addUserGame({ name, steamappid }: { name: string; steamappid?: number }) {
