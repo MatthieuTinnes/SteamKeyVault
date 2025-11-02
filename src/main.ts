e2e92910c5e21ba3aa9a134d344ba5d9f818a955
MatthieuTinnes <matthieu.tinnes@gmail.com>
2025-11-02 21:51:16 +0100

    Refactor styles and remove Tailwind CSS integration
    - Removed Tailwind CSS import from main.css and vite.config.ts.
    - Updated main.ts to directly import PrimeVue and its related styles.
    - Simplified PrimeVue setup by removing custom theme preset configuration.

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,20 @@
 import './assets/main.css'
+import 'primeicons/primeicons.css'
+import 'primevue/resources/themes/lara-light-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import PrimeVue from 'primevue/config';
-import ToastService from 'primevue/toastservice';
-import Aura from '@primeuix/themes/aura';
+import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+
 const app = createApp(App)
 const pinia = createPinia()
-import { definePreset } from '@primeuix/themes';
-const stylePreset = definePreset(Aura, {
-    semantic: {
-        primary: {
-            50: '{blue.50}',
-            100: '{blue.100}',
-            200: '{blue.200}',
-            300: '{blue.300}',
-            400: '{blue.400}',
-            500: '{blue.500}',
-            600: '{blue.600}',
-            700: '{blue.700}',
-            800: '{blue.800}',
-            900: '{blue.900}',
-            950: '{blue.950}'
-        },
-        colorScheme: {
-            dark: {
-                surface: {
-                    0: '#ffffff',
-                    50: '{neutral.50}',
-                    100: '{neutral.100}',
-                    200: '{neutral.200}',
-                    300: '{neutral.300}',
-                    400: '{neutral.400}',
-                    500: '{neutral.500}',
-                    600: '{neutral.600}',
-                    700: '{neutral.700}',
-                    800: '{neutral.800}',
-                    900: '{neutral.900}',
-                    950: '{neutral.950}'
-                }
-            }
-        }
-    }
-});
-app.use(PrimeVue, {
-  theme: {
-    preset: stylePreset,
-    options: {
-      darkModeSelector: false,
-    },
-  },
-})
+
+app.use(PrimeVue)
 app.use(router)
 app.use(pinia)
 app.use(ToastService);
